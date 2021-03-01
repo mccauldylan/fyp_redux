e518c529d2ccf3863abc869c1bb98cb8e5f1a559
mccauldylan <dmccaul@tcd.ie>
2021-03-01 00:27:09 +0000

    react cleanup

---
+++ view/src/components/navbar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+//MUI
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+
+export class navbar extends Component {
+    render() {
+        return (
+           <Appbar>
+               <Toolbar>
+                   <Button color="inherit">
+
+                   </Button>
+               </Toolbar>
+           </Appbar>
+        )
+    }
+}
+
+export default navbar
