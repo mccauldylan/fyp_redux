3f2fd240c57a483c88e791cdab5d03f51c3b46c9
mccauldylan <dmccaul@tcd.ie>
2021-03-03 11:08:26 +0000

    redux user details working, even on refresh

---
+++ view/src/components/Profile.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Paper from "@material-ui/core/Paper";
+import withStyles from "@material-ui/core/styles/withStyles";
+
+class Profile extends Component {
+  render() {
+    const {
+      user: {
+        credentials: { firstName, lastName, username, email, profession },
+        authenticated,
+        loading,
+      },
+    } = this.props;
+
+    let profileMarkup = !loading ? (
+      authenticated ? (
+        <Paper>
+          <div>
+            <h2>{firstName}</h2>
+            <h2>{profession}</h2>
+          </div>
+        </Paper>
+      ) : (
+        <p>placeholder</p>
+      )
+    ) : (
+      <p>loading...</p>
+    );
+
+    return profileMarkup;
+  }
+}
+
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
+
+Profile.propTypes = {
+  user: PropTypes.object.isRequired,
+};
+
+export default connect(mapStateToProps)(withStyles()(Profile));
--- view/src/pages/home.js
+++ view/src/pages/home.js
@@ -1,43 +1,20 @@
-import React, { Component } from 'react'
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
-
-const rows = [
-    "1",
-    "2"
-]
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import Profile from "../components/Profile";
 
 export class home extends Component {
-
-    render() {
-        return (
-            <Grid 
-            container 
-            direction="column"
-            justify="flex-end"
-            alignItems="center">
-                <Grid item xs ={6}>
-                    <Paper>
-                        <Button variant="outlined" color="primary">
-                            1 Register Info
-                        </Button>
-                    </Paper>
-                </Grid> 
-                <Grid item xs ={6}>
-                    <Paper>
-                        <Button variant="outlined" color="primary">
-                            27 Sleeping
-                        </Button>
-                    </Paper>
-                </Grid>   
-            </Grid>
-               
-                
-
-            
-        )
-    }
+  render() {
+    return (
+      <Grid container spacing={20}>
+        <Grid item sm={4}>
+          <Profile />
+        </Grid>
+        <Grid item sm={6}></Grid>
+      </Grid>
+    );
+  }
 }
 
-export default home
+export default home;
