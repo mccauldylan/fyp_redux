30b2b863ffe8fda3e298db24de3008ebe211850f
mccauldylan <dmccaul@tcd.ie>
2021-03-08 12:49:53 +0000

    commit

--- view/src/components/navbar.js
+++ view/src/components/navbar.js
@@ -1,25 +1,29 @@
-import React, { Component } from 'react'
-import Link from 'react-router-dom/Link'
+import React, { Component } from "react";
+import Link from "react-router-dom/Link";
 
 //MUI
-import Appbar from '@material-ui/core/Appbar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
+import Appbar from "@material-ui/core/Appbar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
 
 export class navbar extends Component {
-    render() {
-        return (
-           <Appbar>
-               <Toolbar className="nav-container">
-                   <Button color="inherit" component={Link} to="/login">Login</Button>
-                   <Button color="inherit" component={Link} to="/">Home</Button>
-                   <Button color="inherit" component={Link} to="/signup">Signup</Button>
-                   <Button color="inherit" component={Link} to="/Table">Table</Button>
-
-               </Toolbar>
-           </Appbar>
-        )
-    }
+  render() {
+    return (
+      <Appbar>
+        <Toolbar className="nav-container">
+          <Button color="inherit" component={Link} to="/login">
+            Login
+          </Button>
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="/signup">
+            Signup
+          </Button>
+        </Toolbar>
+      </Appbar>
+    );
+  }
 }
 
-export default navbar
+export default navbar;
