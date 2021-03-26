0706ec84312c13ab0fb4d285d92e25a542dedfca
mccauldylan <dmccaul@tcd.ie>
2021-03-26 14:40:50 +0000

    test

--- view/src/pages/home.js
+++ view/src/pages/home.js
@@ -9,6 +9,7 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Divider from "@material-ui/core/Divider";
+import TableRow from "@material-ui/core/TableRow";
 
 // Redux
 import { connect } from "react-redux";
